5a529680f837983f6145c17e897a669a36c9c863
cristianrperalta <cristian.r.peralta@gmail.com>
2020-10-26 15:29:24 -0300

    Se modifica path de Template Engine

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el 
 
 // ************ Template Engine - (don't touch) ************
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+app.set('views', path.join(__dirname, 'src', 'views')); // Define la ubicación de la carpeta de las Vistas
 
 
 // ************ Route System require and use() ************
