d3875037322f5d7447f9ed22ddb31aaccf4166ac
DaniPinelli <elidaniypri@gmail.com>
2020-10-29 17:46:47 -0300

    Creado productList y agregadas rutas a productDetail y a productList

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,18 +1,23 @@
 module.exports = {
     create: function (req, res, next) {
-    res.render("products/newProduct")
-},
+        res.render('products/newProduct')
+    },
 
-    store : function(req, res){
-    res.redirect('products/productDetail')
-},
+    /*store: function (req, res) {
+        res.redirect('products/productDetail')
+    },*/
 
     edit: function (req, res, next) {
-    res.render("products/editProduct")
-},
+        res.render('products/editProduct')
+    },
 
-    store : function(req, res){
-    res.redirect('products/productDetail')
-},
+    list: function (req, res) {
+        res.render('products/productList')
+    },
+
+
+    detail: function (req, res) {
+        res.render('products/productDetail')
+    }
 
 }
--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,16 @@ const productsController = require('../controllers/productsController');
 
 /*** CREATE ONE PRODUCT ***/
 router.get('/new-product', productsController.create);
-router.post('/', productsController.store);
+//router.post('/', productsController.store);
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/edit-product', productsController.edit);
-router.put('/', productsController.store);
+//router.put('/', productsController.store);
+
+/*** PRODUCT DETAIL ***/
+router.get('/productDetail', productsController.detail);
+
+/*** PRODUCT LIST ***/
+router.get('/productList', productsController.list);
 
 module.exports = router;
