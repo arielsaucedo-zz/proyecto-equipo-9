87d289fb9fe8876a79d702db231117b203ced03b
cristianrperalta <cristian.r.peralta@gmail.com>
2020-11-04 22:31:36 -0300

    Se agregan modificaciones a Register. Validaciones, y parte del proceso de alta de usuario (POST).

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,17 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const usersFilePath = path.join(__dirname, '../data/users.json');
-let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-//const {check, validationResult, } = require('express-validator')
+const fs = require('fs')
+const path = require('path')
+const usersFilePath = path.join(__dirname, '../data/users.json')
+let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+const {check, validationResult, } = require('express-validator')
 
 const controller = {
     login : function(req, res){
         res.render('users/login')
     },
     register : function(req, res){
-        res.render('users/register')
+        res.render('users/register', {errors: []} )
     },
     store : function(req, res){
+        let errors = validationResult(req)
+        console.log(errors)
+        if (!errors.isEmpty()) {
+            return res.render('users/register', {errors: errors.errors} );
+        }
         //hay que agregar validaciones, ver el tema del ID, categoría, la imagen y pass.
         let newUser = {
             id: users.length + 1,
@@ -19,12 +24,13 @@ const controller = {
             last_name: req.body.last_name,
             user_name: req.body.user_name,
             password: req.body.password_confirmation,
-            category: "user",
+            rol: "user",
             image: "user_3.jpg"
         }
         users.push(newUser)
-        let usersString = JSON.stringify(users)
-        fs.writeFileSync(usersFilePath, usersString)
+        //console.log(users)
+        //let usersString = JSON.stringify(users)
+        //fs.writeFileSync(usersFilePath, usersString)
         res.redirect('users/login')
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 // ************ Require's ************
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 
 // ************ Controller Require ************
@@ -10,6 +11,22 @@ router.get('/login', usersController.login);
 
 /*** CREATE ONE USER - REGISTER ***/
 router.get('/register', usersController.register);
-router.post('/', usersController.store); 
+router.post('/', [
+    check('first_name')
+        .isLength( {min: 1})
+        .withMessage('Por favor, ingrese su nombre'),
+    check('last_name')
+        .isLength( {min: 1})
+        .withMessage('Por favor, ingrese su apellido'),
+    check('user_name')
+        .isEmail()
+        .withMessage('Por favor, ingrese una dirección de correo electrónico válida'),
+    check('password_initial')
+        .isLength( {min: 8})
+        .withMessage('La contrase debe contener al menos 8 caracteres'),
+    check('password_confirmation')
+        .isLength( {min: 8})
+// agregar validación de comparación password_initial vs password_confirmation
+], usersController.store); 
 
 module.exports = router;
