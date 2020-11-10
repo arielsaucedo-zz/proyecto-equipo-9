638afaa87e155803b615728a6d3a5ddb9ddf1969
cristianrperalta <cristian.r.peralta@gmail.com>
2020-11-09 21:11:52 -0300

    Arreglo en redireccion de Log-In por cambio de rutas en products

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -28,14 +28,10 @@ const controller = {
                 req.session.user = userLoggedIn.user_name
                 req.session.first_name = userLoggedIn.first_name
                 req.session.last_name = userLoggedIn.last_name
-                console.log('processLogin')
-                console.log(req.session.user)
-                console.log('------')
-                console.log(userLoggedIn.email)
                 // if(req.body.rememberMe){
                 //     res.cookie('rememberMe', user.user_name, { maxAge: 120 * 1000 })
                 // }
-                return res.redirect('../products/productsList')
+                return res.redirect('../products')
             }
         } else {
             return res.render('users/login', { errors: errors.errors } )
