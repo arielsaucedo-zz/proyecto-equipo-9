de367e45536693013d16d589eb1e4baa1ab92bc2
DaniPinelli <elidaniypri@gmail.com>
2020-11-11 09:33:06 -0300

    Solamente eliminados unos comentarios en productsController

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,25 +1,15 @@
  const products = require('../data/products.json');
  const fs = require('fs')
  const path = require('path')
- const {check, validationResult, } = require('express-validator')
+ const {
+     check,
+     validationResult,
+ } = require('express-validator')
  const productsFilePath = path.join(__dirname, '../data/products.json')
 
  let productsController = {
 
      list: function (req, res) {
-         /*    let data = [...products]
-
-             if (req.query.price_min) {
-                 data = data.filter(function (product) {
-                     return product.price >= req.query.price_min
-                 })
-             }
-
-             if (req.query.name) {
-                 data = data.filter(function (product) {
-                     return product.name.includes(req.query.name);
-                 })
-             }*/
 
          res.render('products/list', {
              products: products
@@ -27,13 +17,15 @@
      },
 
      show: function (req, res) {
-        let product = []
-        product = products.filter(function(productElement){
-            if(productElement.id == req.params.id){
-                return true
-            }
-        })
-        res.render('products/productDetail', { productDetail : product[0] })
+         let product = []
+         product = products.filter(function (productElement) {
+             if (productElement.id == req.params.id) {
+                 return true
+             }
+         })
+         res.render('products/productDetail', {
+             productDetail: product[0]
+         })
      },
 
      create: function (req, res, next) {
@@ -42,18 +34,15 @@
 
      store: function (req, res) {
          products.push(
-//             {
-//             ...req.body,
-//             id: products[products.length - 1].id + 1
-//         }
-         {
-            id: products[products.length - 1].id + 1,
-            name: req.body.product_name,
-            detail: req.body.product_description,
-            price: req.body.product_price,
-            category: req.body.product_category,
-            image: "/images/Net-4.jpg" // aca hay que meter la imagen posta (capaz sea necesario hacer una vista aparte)
-          },
+
+             {
+                 id: products[products.length - 1].id + 1,
+                 name: req.body.product_name,
+                 detail: req.body.product_description,
+                 price: req.body.product_price,
+                 category: req.body.product_category,
+                 image: "/images/Net-4.jpg" // aca hay que meter la imagen posta (capaz sea necesario hacer una vista aparte)
+             },
          )
          fs.writeFileSync(productsFilePath, JSON.stringify(products))
 
@@ -61,35 +50,39 @@
      },
 
      edit: function (req, res, ) {
-        let product = []
-        product = products.filter(function(productElement){
-            if(productElement.id == req.params.id){
-                return true
-            }
-        })
-        res.render('products/productEdit', { productEdit : product[0] })
+         let product = []
+         product = products.filter(function (productElement) {
+             if (productElement.id == req.params.id) {
+                 return true
+             }
+         })
+         res.render('products/productEdit', {
+             productEdit: product[0]
+         })
      },
 
      update: function (req, res, next) {
-        let errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.render('products/productEdit', { errors: errors.errors } );
-        }
-        let productEdited = {}
-        productEdited = products.map(function(productElement){
-            if(productElement.id == req.params.id){
-                productElement.name = req.body.name
-                productElement.category = req.body.category
-                productElement.detail = req.body.detail
-                productElement.price = req.body.price
-                productElement.image = req.body.image
-            }
-            return productElement
-        })
-        
-        productEdited = JSON.stringify(productEdited)
-        fs.writeFileSync(productsFilePath, productEdited)
-        res.redirect('..')
+         let errors = validationResult(req)
+         if (!errors.isEmpty()) {
+             return res.render('products/productEdit', {
+                 errors: errors.errors
+             });
+         }
+         let productEdited = {}
+         productEdited = products.map(function (productElement) {
+             if (productElement.id == req.params.id) {
+                 productElement.name = req.body.name
+                 productElement.category = req.body.category
+                 productElement.detail = req.body.detail
+                 productElement.price = req.body.price
+                 productElement.image = req.body.image
+             }
+             return productElement
+         })
+
+         productEdited = JSON.stringify(productEdited)
+         fs.writeFileSync(productsFilePath, productEdited)
+         res.redirect('..')
      },
 
      destroy: function (req, res) {
