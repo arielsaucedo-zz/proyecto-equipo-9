dac2c0401d4da4102503a81444bd307a017ad10b
cristianrperalta <cristian.r.peralta@gmail.com>
2020-11-30 11:41:09 -0300

    Cambios de estilos en general

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,13 @@ const path = require('path')
 const productDataFilePath = path.join(__dirname, '../data/product')
 let productData = require(productDataFilePath)
 
+const products = require('../data/products.json');
+
 module.exports = {
     index: function (req, res, next) {
-        res.render("index")
+        res.render("index", { products: products })
     },
+    search: (req, res) => {
+		res.render('results')
+	},
 }
