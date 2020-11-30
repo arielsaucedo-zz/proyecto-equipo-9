a6c784c96222971897f43146ee6c521d5f7ddd8e
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2020-11-30 17:55:05 -0300

    Modificacion de imagenes y aplicacion de multer en productCreate

--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
+const path = require('path')
+
+// ************ Controller Require ************
 const productsController = require('../controllers/productsController');
+
+// ************ Multer Require ************
+var multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images/uploads_users')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+var upload = multer({ storage: storage });
+
 
 /*** localhost:3000/products/ ***/
 router.get('/', productsController.list);
 
-/*** localhost:3000/products/create ***/
+/*** localhost:3000/products/productCreate ***/
 router.get('/productCreate', productsController.create);
 /*** localhost:3000/products/ ***/
-router.post('/', productsController.store);
+router.post('/', upload.any(), productsController.store);
 
-/*** localhost:3000/products/:id ***/
+
+/*** localhost:3000/products/detail/:id ***/
 //http://localhost:3000/products/detail/4/
 router.get('/detail/:id', productsController.show);
 
@@ -32,6 +49,7 @@ router.delete('/:id/delete', productsController.destroy);
 
 
 /*** PRODUCT CART ***/
+/*** localhost:3000/products/productCart ***/
 router.get('/productCart', productsController.cart);
 
 module.exports = router;
