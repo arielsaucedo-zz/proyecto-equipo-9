08a05ecbc9f02c7aa835eaec6d1e7de7fc322d32
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2020-12-17 10:32:42 -0300

    Modificacion de login header, agrego slider imagenes index, borrado de banner viejo

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'jazmin2014',
+    "password": '',
     "database": "kalitero_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
