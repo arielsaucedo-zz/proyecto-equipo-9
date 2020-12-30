61a9b040ed9273d630f512786b4e30e15cebab44
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2020-12-30 08:34:57 -0300

    Cambios menores de estilos

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'jazmin2014',
+    "password": 'root',
     "database": "kalitero_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
