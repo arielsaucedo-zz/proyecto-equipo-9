656cb61c6c44a6d8e86530b2149ff21a159bdd11
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-01-15 19:18:05 -0300

    Creacion de archivo .ENV

--- database/config/config.js
+++ database/config/config.js
@@ -1,22 +1,24 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": 'root',
-    "database": "kalitero_db",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
