69b9596485ada9b239fddbd5f6cd17f3abdefdc1
DaniPinelli <elidaniypri@gmail.com>
2021-01-24 20:48:03 -0300

    Validacion del formulario de edicion de usuarios

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -1,11 +1,11 @@
 validar = () => {
     let nombre, apellido, email, password, expresion_user,expresion_password, confirm;
-    nombre = document.getElementById('first_name').value; 
-    apellido = document.getElementById('last_name').value;
-    userName = document.getElementById('user_name').value;
-    password = document.getElementById('password_initial').value;
-    confirm = document.getElementById('password_confirmation').value;
-    expresion_user =  /\w+@\w+\.+[a-z]/;
+    nombre = document.getElementById('validationTooltip01').value; 
+    apellido = document.getElementById('validationTooltip02').value;
+    userName = document.getElementById('validationTooltipUsernamePrepend').value;
+    password = document.getElementById('validationTooltip03').value;
+    confirm = document.getElementById('validationTooltip04').value;
+    expresion_user =  /@/;
     expresion_password = /^(?=.*\d).{4,8}$/;
     if(nombre.length < 3) {
         document.getElementById('span_name').style.display = 'block';
@@ -20,7 +20,7 @@ validar = () => {
         return false;
     }
 
-    else if(expresion_password.test(password)) {
+    else if(!expresion_password.test(password)) {
     document.getElementById('span_password').style.display = 'block';
     return false;
     }
