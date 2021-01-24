996b104d730bd61a6e81ad1d11342bb7c18117c9
DaniPinelli <elidaniypri@gmail.com>
2021-01-24 20:18:59 -0300

    Modoficaciones en validaciones de userDetail

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -1,95 +1,31 @@
-window.addEventListener('load', () => {
-    const form = document.getElementById('form');
-    const send = document.getElementById('send');
-
-    const firstName = document.getElementById('validationTooltip01');
-    const lastName = document.getElementById('validationTooltip02');
-    const userName = document.getElementById('uvalidationTooltipUsername');
-    const password = document.getElementById('validationTooltip03');
-    const confirm = document.getElementById('validationTooltip04');
-})
-
-
-const user = {
-    firstName: false,
-    lastName: false,
-    userName: false,
-    image_avatar: false,
-    password: false,
-    confirm: false,
-}
-
-form.addEventListener('send', (e) => {
-    e.preventDefault()
-    validateForm()
-})
-
-firstName.addEventListener("change", (e) => {
-    if (e.target.trim().length > 3) {
-        user.firstName = true;
-        firstName.classList.remove("border-nok");
-        firstName.classList.add("border-ok");
-    } else {
-        firstName.classList.remove("border-ok");
-        firstName.classList.add("border-nok");
-        document.innerHTML('El nombre debe tener al menos 3 letras');
+validar = () => {
+    let nombre, apellido, email, password, expresion_user,expresion_password, confirm;
+    nombre = document.getElementById('first_name').value; 
+    apellido = document.getElementById('last_name').value;
+    userName = document.getElementById('user_name').value;
+    password = document.getElementById('password_initial').value;
+    confirm = document.getElementById('password_confirmation').value;
+    expresion_user =  /\w+@\w+\.+[a-z]/;
+    expresion_password = /^(?=.*\d).{4,8}$/;
+    if(nombre.length < 3) {
+        document.getElementById('span_name').style.display = 'block';
+        return false;
     }
-})
-lastName.addEventListener("change", (e) => {
-    if (e.target.trim().length > 3) {
-        user.lastName = true;
-        lastName.classList.remove("border-nok");
-        lastName.classList.add("border-ok");
-    } else {
-        firstName.classList.remove("border-ok");
-        firstName.classList.add("border-nok");
-        document.innerHTML('El apellido debe tener al menos 3 letras');
+    else if(apellido.length < 3) {
+        document.getElementById('span_last_name').style.display = 'block';
+        return false;
     }
-})
-userName.addEventListener("change", (e) => {
-    if (validator.isEmail(userName.value)) {
-        user.userName = true;
-        userName.classList.remove("border-nok");
-        userName.classList.add("border-ok");
-
-    } else {
-        userName.classList.remove("border-ok");
-        userName.classList.add("border-nok");
-        document.innerHTML('El nombre de usuario debe ser un e-mail')
+    else if(!expresion_user.test(userName)) {
+        document.getElementById('span_user_name').style.display = 'block';
+        return false;
     }
-})
 
-password.addEventListener("change", function () {
-    if (/^(?=.*\d).{4,8}$/.test(password.value)) {
-        password.classList.remove("border-nok")
-        password.classList.add("border-ok")
-        user.password = true;
-    } else {
-        password.classList.remove("border-ok")
-        password.classList.add("border-nok")
-        document.innerHTMLinnerHTML('La contraseña debe contener entre 4 y 8 caracteres e incluir al menos un número')
-
+    else if(expresion_password.test(password)) {
+    document.getElementById('span_password').style.display = 'block';
+    return false;
     }
-})
-
-confirmn.addEventListener("change", function () {
-    if (validator.equals(confirmn.value, password.value)) {
-        confirmn.classList.remove("border-nok")
-        confirmn.classList.add("border-ok")
-        user.confirmn = true;
-
-    } else {
-        confirmn.classList.remove("border-ok")
-        confirmn.classList.add("border-nok")
-        document.innerHTML('La contraseña no coincide')
-
-    }
-})
-
-
-const validateForm = () => {
-    const userValues = Object.values(user)
-    const valid = userValues.findIndex(value => value == false)
-    if (valid == -1) form.send()
-    else alert('Por favor corrija los campos con error')
+    else if(confirm !== password) {
+        document.getElementById('span_confirm').style.display = 'block';
+        return false;
+        }
 }
