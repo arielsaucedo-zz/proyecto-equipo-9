01b45e74a105c557df4b437c56214b6957ae1753
cristianrperalta <cristian.r.peralta@gmail.com>
2021-02-03 22:46:49 -0300

    Se suben APIS de productos y modificaciones a Carrito

--- routes/api/productsApiRouter.js
+++ routes/api/productsApiRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const apiProductsController = require('../../controllers/api/productsApiController');
 
-
+router.get('/', apiProductsController.showAll)
+router.get('/:id', apiProductsController.find)
 
 module.exports = router;
